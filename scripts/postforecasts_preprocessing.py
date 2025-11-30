eedf1caa5b2951e3b9dd461ed6d5e8b5caaae1ca
gkchln <121186725+gkchln@users.noreply.github.com>
2025-11-30 17:54:49 +0100

    Update postforecasts preprocessing script

--- scripts/postforecasts_preprocessing.py
+++ scripts/postforecasts_preprocessing.py
@@ -1,12 +1,14 @@
+import os
 import pandas as pd
 
 test_start = '2024-01-01 00:00:00'
 test_end = '2024-12-31 23:00:00'
 
-prices_true_path = 'data/source/prices.csv'
-prices_pred_path = 'data/output/benchmarks/prices_pred_LEAR.csv'
+prices_true_path = os.path.join('data', 'source', 'mgp_nat_price.csv')
+prices_pred_path = os.path.join('data', 'output', 'price-based', 'prices', 'full_none_1237_conc_none_017_none_364_aic_20240101_20241231.csv')
+output_folder = os.path.join('data', 'processed', 'postforecasts', 'fARX')
 
-output_path = 'data/processed/postforecasts/lear/'
+os.makedirs(output_folder, exist_ok=True)
 
 timestamps = pd.date_range(test_start, test_end, freq='h', name='date')
 
@@ -21,4 +23,4 @@ df.index = timestamps
 for h in range(24):
     df_h = df[df.index.hour == h]
     df_h.index = df_h.index.strftime("%Y%m%d").astype(int)
-    df_h.to_csv(output_path + f'h{h}.csv', index=True)
+    df_h.to_csv(os.path.join(output_folder, f'h{h}.csv'), index=True)
