ac3cf99e362a281588359bff6cd0d78eb45cbb02
gkchln <121186725+gkchln@users.noreply.github.com>
2026-04-30 12:03:49 +0200

    Add EPEXCurvesConstructor class for EPEX curve preprocessing and dataset generation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import holidays
 from scipy.interpolate import interp1d
-from src.utils import get_timestamp_from_gme_system, fix_daylight_saving_time
+from .utils import get_timestamp_from_gme_system, fix_daylight_saving_time
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -215,6 +215,123 @@ class GMECurvesConstructor:
 
         return data_matrix, grid_points
     
+
+
+class EPEXCurvesConstructor:
+    """
+    Class to handle GME curves preprocessing.
+    Args:
+        market (str, optional): The market type. Defaults to 'MGP'.
+        qty_unit (str, optional): Quantity unit for curves. Defaults to 'GW'.
+    """
+    def __init__(self, qty_unit='GW', price_domain=(-500, 1000), n_prices=1501, float_precision='float32'):
+        self.qty_unit = qty_unit
+        self.price_domain = price_domain
+        self.n_prices = n_prices
+        self.float_precision = float_precision
+    
+    def _get_qty_function(self, steps, xnew, side):
+        """
+        Interpolates cumulative quantity values at specified price points using stepwise interpolation.
+        This function sorts the input DataFrame by the 'Prezzo' column, normalizes the 'cumQuantita' column
+        based on the unit (GW or not), and creates a stepwise interpolation function. The interpolation
+        direction (next or previous) is determined by the 'type' argument.
+        Args:
+            steps (pd.DataFrame): DataFrame containing at least 'Prezzo' and 'cumQuantita' columns.
+            xnew (array-like): New price points at which to interpolate the cumulative quantity.
+            type (str): Type of step interpolation. If 'BID', uses 'next' step; otherwise, uses 'previous'.
+        Returns:
+            np.ndarray: Interpolated cumulative quantity values at the specified price points.
+        Raises:
+            KeyError: If 'Prezzo' or 'cumQuantita' columns are missing in the input DataFrame.
+            ValueError: If 'type' is not recognized.
+        """
+        x = steps['price']
+        if self.qty_unit == 'GW':
+            y = steps['quantity'] / 1000
+        else:
+            y = steps['quantity']
+            
+        if side == 'BID':
+            kind = 'next'
+        else:
+            kind = 'previous'
+            
+        fill_value = (y.iloc[0], y.iloc[-1])
+        f = interp1d(
+            x, y,
+            kind=kind,
+            fill_value=fill_value,
+            bounds_error=False,
+            assume_sorted=True
+        )
+        return f(xnew)
+    
+
+    def get_curves_dataset(self, input_df: pd.DataFrame, side: str, price_grid: np.ndarray | None = None, progress_bar=True):
+        """
+        Generates a dataset of curve values over a specified domain for each unique (Data, Ora) pair in the input DataFrame.
+
+        Args:
+            input_df (pd.DataFrame): Input DataFrame containing at least 'Data', 'Ora', and 'ZonaMercato' columns.
+            type (str): Type of curve to process (e.g., 'OFF', etc.).
+            balance_df (pd.DataFrame): DataFrame containing balance information for import/export adjustments.
+            progress_bar (bool, optional): Whether to display a progress bar during processing. Defaults to True.
+
+        Returns:
+            Tuple[pd.DataFrame, np.ndarray]: 
+                - DataFrame where each row corresponds to a (Data, Ora) timestamp and each column to a grid point in the domain, containing the evaluated curve values.
+                - Numpy array of grid points used for curve evaluation.
+
+        Raises:
+            Any exceptions raised by called methods such as `get_curve_steps`, `add_import_export`, `get_qty_function`, or `fix_daylight_saving_time`.
+
+        Notes:
+            - Handles daylight saving time adjustments on the resulting DataFrame.
+            - If `type` is 'OFF', import/export adjustments are applied to the curve steps.
+        """
+        df = input_df.copy()
+        df = df[df.side == side]
+        df.drop(['side'], axis=1, inplace=True)
+
+        if side == 'BID':
+            df.drop_duplicates(subset=['timestamp', 'price'], keep='first', inplace=True)
+        else:
+            df.drop_duplicates(subset=['timestamp', 'price'], keep='last', inplace=True)
+
+        df = df.set_index("timestamp")
+
+        timestamps = df.index.unique()
+        n = len(timestamps)
+
+        if price_grid is not None:
+            # Checking whether the provided price grid is coherent with the constructor object attributes
+            assert tuple(price_grid[[0, -1]]) == self.price_domain and len(price_grid) == self.n_prices
+            grid_points = price_grid.copy()
+        else:
+            # otherwise we build a classic uniform grid
+            grid_points = np.linspace(self.price_domain[0], self.price_domain[1], self.n_prices)
+
+        data_matrix = np.zeros((n, self.n_prices))
+        idx = 0
+
+        for ts in tqdm(
+            timestamps,
+            disable=not progress_bar
+        ):
+            data_matrix[idx, :] = self._get_qty_function(df.loc[ts], grid_points, side)
+            idx += 1
+
+        # Necessary to transform into DataFrame to use fix_daylight_saving_time
+        data_matrix = pd.DataFrame(data_matrix, index=timestamps)
+        data_matrix = fix_daylight_saving_time(data_matrix)
+
+        # Compression
+        self.timestamps = data_matrix.index # Store timestamps as an attribute
+        data_matrix = data_matrix.to_numpy().astype(self.float_precision)
+        grid_points = grid_points.astype(self.float_precision)
+
+        return data_matrix, grid_points
 
 
 class ExogPreprocessor:
