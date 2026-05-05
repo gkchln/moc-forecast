7ce5093062dd52c814a83ad67afd49015f09f433
gkchln <121186725+gkchln@users.noreply.github.com>
2026-05-05 15:58:35 +0200

    Capitalize weekday names in weekday_mapping for consistency

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,13 +13,13 @@ pd.options.mode.chained_assignment = None  # default='warn'
 GME_DATASET_NAME = 'DomandaOfferta'
 
 weekday_mapping = {
-    0: "monday",
-    1: "tuesday",
-    2: "wednesday",
-    3: "thursday",
-    4: "friday",
-    5: "saturday",
-    6: "sunday"
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday"
 }
 
 class GMECurvesConstructor:
