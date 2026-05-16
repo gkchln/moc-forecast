2e5c1a269b7e9723df31b2ad7845e686214c62d2
gkchln <121186725+gkchln@users.noreply.github.com>
2026-05-16 16:36:44 +0200

    Specify exog use in price-based forecast

--- scripts/price_based/point_forecast.py
+++ scripts/price_based/point_forecast.py
@@ -16,6 +16,8 @@ LAGS_ENDOG = [1, 2, 3, 7]
 EXOG_USE = {
     'Load': {"lags": [0, 1, 7], "structure": "{exog_structure}"},
     'RES': {"lags": [0, 1, 7], "structure": "{exog_structure}"},
+    'NTC FR > IT': {"lags": [0, 1, 7], "structure": "{exog_structure}"},
+    'NTC CH > IT': {"lags": [0, 1, 7], "structure": "{exog_structure}"},
     'Gas': {"lags": [2], "structure": "concurrent"},
     'Coal': {"lags": [2], "structure": "concurrent"},
     'Oil': {"lags": [2], "structure": "concurrent"},
