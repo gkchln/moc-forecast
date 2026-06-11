9ed969bca4ea453c8e5874d137fa052bf318837e
gkchln <121186725+gkchln@users.noreply.github.com>
2026-06-11 17:34:09 +0200

    Change naming convention of curve-based model runs

--- scripts/curve_based/point_forecast.py
+++ scripts/curve_based/point_forecast.py
@@ -13,15 +13,13 @@ import argparse
 ### Fixed parameters ###
 
 LAGS_ENDOG = [1, 2, 3, 7]
+LAGS_EXOG = [0, 1, 7]
+EXOG_STRUCTURE = 'concurrent'
 EXOG_USE = {
-    'Load': {"lags": [0, 1, 7], "structure": "concurrent"},
-    'RES': {"lags": [0, 1, 7], "structure": "concurrent"},
-    'NTC FR > IT': {"lags": [0, 1, 7], "structure": "concurrent"},
-    'NTC CH > IT': {"lags": [0, 1, 7], "structure": "concurrent"},
-    'Gas': {"lags": [2], "structure": "concurrent"},
-    'Coal': {"lags": [2], "structure": "concurrent"},
-    'Oil': {"lags": [2], "structure": "concurrent"},
-    'CO2': {"lags": [2], "structure": "concurrent"}
+    'Load': {"lags": LAGS_EXOG, "structure": EXOG_STRUCTURE},
+    'RES': {"lags": LAGS_EXOG, "structure": EXOG_STRUCTURE},
+    'NTC FR > IT': {"lags": LAGS_EXOG, "structure": EXOG_STRUCTURE},
+    'NTC CH > IT': {"lags": LAGS_EXOG, "structure": EXOG_STRUCTURE},
 }
 CRITERION = 'aic'
 RANDOM_STATE = 42
@@ -45,11 +43,13 @@ def main(
     """Run the daily recalibration forecast pipeline."""
 
     ### Setup logging ###
-    run_name = "{autocorr_struc}_{crosscorr_struc}_{lags_endog}_{transformer}" \
-    "_{K_supply}_{K_demand}_{choice_K}_{calib_wind}_{criterion}_{test_start}_{test_end}".format(
+    run_name = "{autocorr_struc}_{crosscorr_struc}_{lags_endog}_{exog_struc}_{lags_exog}_{transformer}" \
+    "_{trans_recal}_{K_supply}_{K_demand}_{choice_K}_{calib_wind}_{criterion}_{test_start}_{test_end}".format(
         autocorr_struc = str(autocorr_structure).lower()[:4], # 'conc' or 'full'
         crosscorr_struc = str(crosscorr_structure).lower()[:4], # 'conc', 'full' or 'none'
         lags_endog = ''.join(map(str, LAGS_ENDOG)), # e.g. '1237' for lags 1, 2, 3 and 7
+        exog_struc = str(EXOG_STRUCTURE).lower()[:4], # 'conc' or 'full'
+        lags_exog = ''.join(map(str, LAGS_EXOG)), # e.g. '017' for lags 0, 1 and 7
         transformer = transformer, # 'fpca' or 'zst'
         trans_recal = 'dynamic' if recalibrate_transformer else 'static',
         K_supply = K_supply or 'none',
